d0ceae4ad2ea9d3f3b60922df692910a7a8d6db3
lizixing <lzxms923@163.com>
2015-11-06 10:46:21 +0800

    add source with conditional macro

---
+++ source_concompile/input/yuv.h
@@ -0,0 +1,101 @@
+/*****************************************************************************
+ * Copyright (C) 2013 x265 project
+ *
+ * Authors: Steve Borho <steve@borho.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ * This program is also available under a commercial proprietary license.
+ * For more information, contact us at license @ x265.com.
+ *****************************************************************************/
+
+#ifndef X265_YUV_H
+#define X265_YUV_H
+
+#include "input.h"
+#include "threading.h"
+#include <fstream>
+
+#define QUEUE_SIZE 5
+
+namespace X265_NS {
+// private x265 namespace
+#if (!YUV_THREAD_DEL)
+class YUVInput : public InputFile, public Thread
+#else
+class YUVInput : public InputFile
+#endif
+{
+protected:
+
+    int width;
+
+    int height;
+
+    int colorSpace; //< source Color Space Parameter
+
+    uint32_t depth;
+
+    uint32_t framesize;
+
+    bool threadActive;
+
+#if (!YUV_THREAD_DEL)
+    ThreadSafeInteger readCount;
+
+    ThreadSafeInteger writeCount;
+#endif
+//#else
+
+    char* buf[QUEUE_SIZE];
+
+    std::istream *ifs;
+
+    int guessFrameCount();
+#if (!YUV_THREAD_DEL)
+    void threadMain();
+
+    bool populateFrameQueue();
+#endif
+public:
+
+    YUVInput(InputFileInfo& info);
+
+    virtual ~YUVInput();
+
+    void release();
+
+    bool isEof() const                            { return ifs && ifs->eof();  }
+
+    bool isFail()                                 { return !(ifs && !ifs->fail() && threadActive); }
+
+#if (!YUV_THREAD_DEL)
+    void startReader();
+
+    bool readPicture(x265_picture&);
+#endif
+
+#if (YUV_THREAD_DEL)
+	bool lzx_readPicture(x265_picture&);
+#endif
+    const char *getName() const                   { return "yuv"; }
+
+    int getWidth() const                          { return width; }
+
+    int getHeight() const                         { return height; }
+};
+}
+
+#endif // ifndef X265_YUV_H
